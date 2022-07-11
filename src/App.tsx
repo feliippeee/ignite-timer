fba6fb1d8f32302f7e6a0bf0085c548437568ac0
feliippeee <felipe.tecnosuporte@gmail.com>
2022-07-11 03:19:16 -0300

    Páginas e rotas - Layout de rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
-import { useState } from 'react'
+import { ThemeProvider } from 'styled-components'
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router'
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
 
 export function App() {
   return (
-   <h1>hello word</h1>
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+        <GlobalStyle />
+    </ThemeProvider>
   )
 }
-
