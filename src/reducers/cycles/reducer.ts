3158c9e46fa94c3f334b856339da1d6c3b442731
feliippeee <felipe.tecnosuporte@gmail.com>
2022-07-16 00:47:23 -0300

    Reducers - Separando Actions

--- src/reducers/cycles/reducer.ts
+++ src/reducers/cycles/reducer.ts
@@ -1,3 +1,5 @@
+import { ActionTypes } from "./actions";
+
 export interface Cycle {
     id: string;
     task: string;
@@ -11,13 +13,6 @@ interface CycleState {
     cycles: Cycle[];
     activeCycleId: string | null;
   }
-
-export enum ActionTypes {
-    ADD_NEW_CYCLE = 'ADD_NEW_CYCLE',
-    INTERRUPT_CURRENT_CYCLE = 'INTERRUPT_CURRENT_CYCLE',
-    MARK_CURRENT_CYCLE_AS_FINISHED = 'MARK_CURRENT_CYCLE_AS_FINISHED',
-
-}
 
 export function cyclesReducer(state: CycleState, action: any) {
     switch(action.type) {
